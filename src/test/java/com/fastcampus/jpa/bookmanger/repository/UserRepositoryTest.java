5f54d38e04887950f3d403eea899f370be354165
tintoll <belselios@gmail.com>
2023-08-23 22:16:32 +0900

    JpaRepository 기본 메서드 동작 확인

--- src/test/java/com/fastcampus/jpa/bookmanger/repository/UserRepositoryTest.java
+++ src/test/java/com/fastcampus/jpa/bookmanger/repository/UserRepositoryTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.transaction.Transactional;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -16,7 +19,11 @@ class UserRepositoryTest {
     @Test
     void crud() {
         userRepository.save(new User());
-
         userRepository.findAll().forEach(System.out::println);
+
+        User user = userRepository.findById(1L).orElse(null);
+//        User user = userRepository.getOne(1L); // @Transactional 을 해줘야 통과됨.
+        System.out.println(user);
+
     }
 }
